98f845a52e7a82d00b477bdc9a03cca3e78efe6e
SumonaShimu <sumona59shimu@gmail.com>
2023-06-27 10:30:09 +0530

    modified

---
+++ components/About.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <section>
+            about section added
+        </section>
+    );
+};
+
+export default About;
---
+++ components/Projects.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Projects = () => {
+    return (
+        <section>
+            projects
+        </section>
+    );
+};
+
+export default Projects;
